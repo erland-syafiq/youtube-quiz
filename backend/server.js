be3357b1244610694893716735e766e9cc8b0f20
Erland A Syafiq <syafiqeas@gmail.com>
2024-02-07 16:54:21 -0500

    Added Chat GPT Integration

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ import { YoutubeTranscript } from 'youtube-transcript';
 import express from 'express';
 import * as dotenv from 'dotenv';
 import OpenAI from 'openai';
+import fs from 'node:fs/promises';
+
 dotenv.config();
 const app = express();
 const PORT = 8080;
@@ -30,12 +32,12 @@ app.get("/quiz", async (req, res) => {
     try {
         const transcriptJSON = await YoutubeTranscript.fetchTranscript("https://www.youtube.com/watch?v=n2Fluyr3lbc");
         const transcript = decodeTranscript(transcriptJSON);
-        const question = await getMultipleChoice(transcript);
-        res.send(question);
+        const question = await getTestQuestion(transcript);
+        
+        res.json(question);
 
     }
     catch (e) {
-        console.log("Client sent invalid URL");
         console.log(e);
         res.status(400).json({
             "message": "Invalid url"
@@ -65,13 +67,37 @@ app.get('/', (req, res) => {
  * @returns {json}
  */
 async function getMultipleChoice(transcript) {
+    const systemMessage = {
+        "role": "system", 
+        "content": "You help create multiple choice questions from a transcript. The transcript came from a Youtube Video about the topic. Create questions that are technical and whose answers can be found in the transcript. Output in JSON."
+    }
+
     const chatCompletion = await openai.chat.completions.create({
-        messages: [{role: 'user', content: 'Say this is a test'}],
-        model: 'gpt-3.5-turbo'
+        messages: [systemMessage,
+            {role: 'user', content: transcript}],
+        model: 'gpt-3.5-turbo-0125',
+        response_format :{type: "json_object"}
     })
     console.log(chatCompletion.choices[0])
-    return chatCompletion;
+
+    const questionString = chatCompletion.choices[0].message.content;
+    const question = JSON.parse(questionString)
+
+    return question;
 }
+
+/**
+ * Creates a test question for development
+ * @param {string} transcript 
+ * @returns {string}
+ */
+async function getTestQuestion(transcript) {
+    const data = await fs.readFile("test-question.json", {encoding :'utf8'});
+    return data;
+
+}
+
+
 /**
  * Decodes the transcript list object and combines it all into one string
  * @param {list} transcriptList 
@@ -79,8 +105,11 @@ async function getMultipleChoice(transcript) {
  */
 function decodeTranscript(transcriptList) {
     var s = "";
-    transcriptList.forEach((transcriptSegment) => {
+    transcriptList.every((transcriptSegment) => {
         s += transcriptSegment["text"];
+        s += " "
+        
+        return s.length < 1000;
     });
     return s;
 }
