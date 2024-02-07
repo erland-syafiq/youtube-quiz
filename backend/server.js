9de0da6243cb3cbf23f21ca091f959e5e8559e58
Erland A Syafiq <syafiqeas@gmail.com>
2024-02-07 12:32:18 -0500

    Added Finding the Youtube Transcription

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,37 @@
-const express = require('express');
+/**
+ * Backend for Youtube Quizzer App
+ * 
+ * Takes in a youtube video id and creates a quiz from it
+ * 
+ * @author Erland A. Syafiq
+ * 
+ */
+
+import { YoutubeTranscript } from 'youtube-transcript';
+
+import express from 'express';
 const app = express();
 const PORT = 8080;
+
+// n2Fluyr3lbc
+/**
+ * 
+ */
+app.get("/decode", async (req, res) => {
+    try {
+        
+        const transcript = await YoutubeTranscript.fetchTranscript("n2Fluyr3lbc");
+        res.send(transcript);
+    }
+    catch (e) {
+        console.log("Client sent invalid URL");
+        res.status(400).json({
+            "message": "Invalid url"
+        });
+        
+    }
+    
+})
 
 app.get('/test', async (req, res) => {
     factsResponse = await fetch("https://cat-fact.herokuapp.com/facts");
@@ -9,7 +40,7 @@ app.get('/test', async (req, res) => {
         res.json(data);
     }
     else {
-        console.log(res)
+        console.log(factsResponse)
     }
 })
 
