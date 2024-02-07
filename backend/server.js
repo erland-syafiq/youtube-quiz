e5c01d11d2da80a3b594fe65c0eca04310060189
Erland A Syafiq <syafiqeas@gmail.com>
2024-02-07 13:22:27 -0500

    Added Chat GPT Test

--- backend/server.js
+++ backend/server.js
@@ -7,28 +7,39 @@
  * 
  */
 
+// Import statements
 import { YoutubeTranscript } from 'youtube-transcript';
-
 import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
+dotenv.config();
 const app = express();
 const PORT = 8080;
 
-// n2Fluyr3lbc
+// Set API keys
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'],
+})
+
+
+// https://www.youtube.com/watch?v=n2Fluyr3lbc
 /**
  * 
  */
-app.get("/decode", async (req, res) => {
+app.get("/quiz", async (req, res) => {
     try {
-        
-        const transcript = await YoutubeTranscript.fetchTranscript("n2Fluyr3lbc");
-        res.send(transcript);
+        const transcriptJSON = await YoutubeTranscript.fetchTranscript("https://www.youtube.com/watch?v=n2Fluyr3lbc");
+        const transcript = decodeTranscript(transcriptJSON);
+        const question = await getMultipleChoice(transcript);
+        res.send(question);
+
     }
     catch (e) {
         console.log("Client sent invalid URL");
+        console.log(e);
         res.status(400).json({
             "message": "Invalid url"
         });
-        
     }
     
 })
@@ -47,5 +58,31 @@ app.get('/test', async (req, res) => {
 app.get('/', (req, res) => {
     res.send("Welcome to the Youtube Quizzer API!");
 })
+
+/**
+ * Creates a structured question and answer json form a transcript
+ * @param {string} transcript 
+ * @returns {json}
+ */
+async function getMultipleChoice(transcript) {
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{role: 'user', content: 'Say this is a test'}],
+        model: 'gpt-3.5-turbo'
+    })
+    console.log(chatCompletion.choices[0])
+    return chatCompletion;
+}
+/**
+ * Decodes the transcript list object and combines it all into one string
+ * @param {list} transcriptList 
+ * @returns 
+ */
+function decodeTranscript(transcriptList) {
+    var s = "";
+    transcriptList.forEach((element) => {
+        s += element["text"];
+    });
+    return s;
+}
 
 app.listen(PORT);
