9709b78fc8e21c0569d6448c9c5254f1e220db63
Erland A Syafiq <syafiqeas@gmail.com>
2024-02-08 16:42:07 -0500

    Added Default Colors

---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <div><h1>AI Video Quizzer</h1></div>
+        </div>
+    );
+};
+
+export default Navbar;
