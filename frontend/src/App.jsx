7d4e3d0ebcaa05946c1abc40cafa1d4ff8aaf7d7
Erland A Syafiq <syafiqeas@gmail.com>
2024-02-08 19:55:10 -0500

    Added Loading Animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
+import Chat from './components/Chat.jsx';
 
 function App() {
     return (
         <>
             <Navbar/>
+            <Chat/>
         </>
     );
 };
---
+++ frontend/src/components/Chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Chat.module.css';
+import loading from '../assets/loading.svg';
+
+function Chat() {
+    return (
+        <div className={styles.chat}>
+            
+                <img src={loading} className={styles.loading}></img>
+                <h2 className={styles.loadingSubtitle}>Loading transcript...</h2>
+            
+        </div>
+    );
+};
+
+export default Chat;
